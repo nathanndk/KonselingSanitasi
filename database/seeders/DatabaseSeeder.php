fb7e10c3ac2da4300856830486562eb4f9e9180a
christianjoshua <Razor091203@gmail.com>
2024-10-19 12:35:37 +0700

    make an admin role and only admin can access

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,18 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@test.com',
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $user1->assignRole($role);
     }
 }
