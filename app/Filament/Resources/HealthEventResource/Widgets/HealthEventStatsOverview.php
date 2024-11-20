7b9104b38273d19d9cb763c85d7b1b76db07b89d
christianjoshua <Razor091203@gmail.com>
2024-11-20 22:04:29 +0700

    perbaiki stats event

--- app/Filament/Resources/HealthEventResource/Widgets/HealthEventStatsOverview.php
+++ app/Filament/Resources/HealthEventResource/Widgets/HealthEventStatsOverview.php
@@ -9,12 +9,19 @@ use Illuminate\Support\Carbon;
 
 class HealthEventStatsOverview extends BaseWidget
 {
+    /**
+     * Set widget to span full width.
+     */
+    protected static bool $canSpanColumns = true;
+
     protected function getStats(): array
     {
         // Mendapatkan event terdekat berdasarkan tanggal hari ini atau mendatang
         $nextEvent = HealthEvent::where('event_date', '>=', Carbon::today())
             ->orderBy('event_date', 'asc')
             ->first();
+
+        $eventsCount = HealthEvent::where('event_date', '>=', Carbon::today())->count();
 
         return [
             Stat::make('Acara Terdekat', $nextEvent?->title ?? 'Tidak Ada Event')
@@ -25,7 +32,24 @@ class HealthEventStatsOverview extends BaseWidget
                         : 'Belum ada event terjadwal'
                 )
                 ->icon('heroicon-o-calendar')
-                ->color($nextEvent ? 'success' : 'secondary'),
+                ->color($nextEvent ? 'success' : 'secondary')
+                ->extraAttributes([
+                    'class' => 'cursor-pointer hover:bg-gray-50 transition',
+                    'wire:click' => "\$dispatch('setEventFilter', { filter: 'upcoming' })",
+                ]),
+
+            Stat::make('Total Acara Mendatang', $eventsCount)
+                ->description(
+                    $eventsCount > 0
+                        ? "Ada $eventsCount acara yang dijadwalkan."
+                        : 'Belum ada acara yang dijadwalkan.'
+                )
+                ->icon('heroicon-o-calendar')
+                ->color($eventsCount > 0 ? 'primary' : 'secondary')
+                ->extraAttributes([
+                    'class' => 'cursor-pointer hover:bg-gray-50 transition',
+                    'wire:click' => "\$dispatch('setEventFilter', { filter: 'all' })",
+                ]),
         ];
     }
 }
