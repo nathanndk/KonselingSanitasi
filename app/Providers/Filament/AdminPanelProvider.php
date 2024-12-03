b94a91c047fedd0ba109c59a46ca12f0ca1c8911
christianjoshua <Razor091203@gmail.com>
2024-12-03 20:28:01 +0700

    update font

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -39,7 +39,7 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Yellow,
             ])
             ->databaseNotifications()
-            ->font('Open Sans')
+            ->font('Product Sans')
             ->brandName('Dinas Kesehatan')
             ->brandLogo(asset('images/logo_dinkes.png'))
             ->favicon(asset('images/logo_dinkes.png'))
